b240219b9849e17e81a11a978ce2ee2423313293
Dani Laney <hiideeho@gmail.com>
2026-05-20 06:46:21 -0500

    submission

---
+++ src/main/java/org/example/Album.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Album extends LibraryItem {
+    protected int trackCount;
+
+    public Album(String title, String author, int year, int trackCount) {
+        super(title, author, year);
+        this.trackCount = trackCount;
+    }
+
+    public String toString() {return "Album: " + title + " by " + author + " (" + year + ") - " + trackCount + " tracks";}
+    public int getTrackCount() {return trackCount;}
+}
